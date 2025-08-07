81eefb22bb92179e4812c5ba9e6f47082c54a86b
i18n <i18n.site@gmail.com>
2025-08-07 17:12:06 +0800

    🔥

---
+++ udeps.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# 检测未使用的依赖
+
+DIR=$(realpath $0) && DIR=${DIR%/*/*}
+cd $DIR
+
+set -ex
+
+if ! hash cargo-machete 2>/dev/null; then
+  cargo install cargo-machete
+fi
+
+RUST_LOG=warn cargo machete --fix
+
+# if ! hash cargo-udeps 2>/dev/null; then
+#   cargo install cargo-udeps --locked
+# fi
+#
+# cargo +nightly udeps --workspace --all-features --output json | direnv exec . ./udeps.coffee
+./protoGen.sh
